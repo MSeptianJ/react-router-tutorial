aba0ef7e1d336adb137cf4e1e6974fb6c647ae16
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-01-09 21:26:23 +0700

    Edit Contact

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
 import ErrorPage from './error-page';
 import './index.css';
-import Root from './routes/root';
-import Contact from './routes/contact';
+import Root, {
+	loader as rootLoader,
+	action as rootAction,
+} from './routes/root';
+import Contact, { loader as contactLoader } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Root />,
-		errorElement: <ErrorPage />,
-		children: [
-			{
-				path: 'contacts/:contactId',
-				element: <Contact />,
-			},
-		],
-	},
-]);
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route
+			path="/"
+			element={<Root />}
+			errorElement={ErrorPage}
+			loader={rootLoader}
+			action={rootAction}
+		>
+			<Route
+				path="contacts/:contactId"
+				element={<Contact />}
+				errorElement={ErrorPage}
+				loader={contactLoader}
+			/>
+			<Route
+				path="contacts/:contactId/edit"
+				element={<EditContact />}
+				loader={contactLoader}
+				action={editAction}
+			/>
+		</Route>
+	)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
